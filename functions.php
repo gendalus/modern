4a8d820c5d9386b20a505a906519b0c2553fd498
gendalus <gendalus@gmail.com>
2018-04-16 16:15:57 +0200

    feat: removed emoji conversion

--- functions.php
+++ functions.php
@@ -187,3 +187,9 @@
 		if ( class_exists( 'OCDI_Plugin' ) && is_admin() ) {
 			require MODERN_PATH_PLUGINS . 'one-click-demo-import/one-click-demo-import.php';
 		}
+
+        remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+        remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+        remove_action( 'wp_print_styles', 'print_emoji_styles' );
+        remove_action( 'admin_print_styles', 'print_emoji_styles' );
+?>
